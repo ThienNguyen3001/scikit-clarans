c3c9aeac6d91699e95ec0c47e3b91a7b68184641
ThienNguyen3001 <thiennguyen03001@gmail.com>
2026-09-16 12:56:17 +0700

    update 0.3.0

--- clarans/__init__.py
+++ clarans/__init__.py
@@ -2,7 +2,7 @@ from ._clarans import CLARANS
 from ._fast_clarans import FastCLARANS
 from .utils import EfficiencyWarning, HAS_CYTHON, calculate_cost, check_medoids
 
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 __all__ = [
     "CLARANS",
     "FastCLARANS",
